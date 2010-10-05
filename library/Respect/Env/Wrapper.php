95ede0a1a028f4b358ba6435f564e975d267c021
Alexandre Gomes Gaigalas <alexandre@gaigalas.net>
2010-10-04 21:13:33 -0300

    The good and the evil, bwuahahaha

--- library/Respect/Env/Wrapper.php
+++ library/Respect/Env/Wrapper.php
@@ -27,6 +27,51 @@ abstract class Wrapper
         return self::$current;
     }
 
+    public static function evil($namespace, $force=false)
+    {
+        if ($force || class_exists('\PHPUnit_Framework_TestCase'))
+            eval(
+                'namespace ' . $namespace . '; 
+            use Respect\Env\Wrapper;
+            function getenv($varname)
+            {
+                return Wrapper::getCurrent()->getenv($varname);
+            }
+
+            function putenv($setting)
+            {
+                return Wrapper::getCurrent()->putenv($setting);
+            }
+
+            function shell_exe($command)
+            {
+                return Wrapper::getCurrent()->shell_exec($command);
+            }
+
+            function sys_get_temp_dir()
+            {
+                return Wrapper::getCurrent()->sys_get_temp_dir();
+            }
+
+            function filter_input($type, $variable_name, $filter=FILTER_DEFAULT,
+                $options=null)
+            {
+                return Wrapper::getCurrent()->filter_input($type, $variable_name, $filter,
+                    $options);
+            }
+
+            function filter_input_array($type, $definition=null)
+            {
+                return Wrapper::getCurrent()->filter_input_array($type, $definition);
+            }
+
+            function filter_has_var($type, $variable_name)
+            {
+                return Wrapper::getCurrent()->filter_has_var($type, $variable_name);
+            }'
+            );
+    }
+
 }
 
 function getenv($varname)
